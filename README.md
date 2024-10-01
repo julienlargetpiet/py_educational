47015565dab192ab2addef6ea6d74fbd02f04057
Julien <pgyygp090@gmail.com>
2024-10-01 17:19:59 +0200

    added multiple_groups.py

--- README.md
+++ README.md
@@ -10,5 +10,16 @@ Associated file:
 
 Output all the possible pairs to do with all elements in a list.
 
-Modify the input list at the first line of the file.
+Modify the **input list** at the first line of the file.
+
+## multiple_groups
+
+Associated file:
+
+`multiple_groups.py`
+
+Output all possible groups to make with a set of elements inside a list.
+
+Modify the **input list** and the **group size** at the first line of the beginning of the file.
+
 
---
+++ multpile_groups.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+cur_lst= deque(["Marc", "Sylvie", "Julien", "Christine", "Axel"])
+cur_lst2 = deque([])
+for i in cur_lst: cur_lst2.append(i)
+Pre_len = len(cur_lst)
+rtn_lst = []
+group_size = 3
+
+if group_size > 1:
+    Cnt = 1
+    while Cnt < group_size:
+        rtn_lst = []
+        for i in cur_lst2:
+            cnt = 0
+            cur_lst.reverse()
+            for k, x in enumerate(cur_lst):
+                if i in x: break
+            k = len(cur_lst) - k
+            cur_lst.reverse()
+            while k + cnt < len(cur_lst):
+                rtn_lst.append(f"{i}-{cur_lst[cnt + k]}")
+                cnt += 1
+        cur_lst = deque([])
+        for i in rtn_lst: cur_lst.append(i)
+        Cnt += 1
+    print(rtn_lst)
+else:
+    print(cur_lst)
